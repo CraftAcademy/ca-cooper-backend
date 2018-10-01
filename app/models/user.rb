2549432d2a1cf160e597d718734f800c28007829
oliverochman <oliver.ochman@hotmail.com>
2018-10-01 10:23:41 +0200

    clean up user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
-# frozen_string_literal: true
-
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # Include default devise modules.
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
 end
