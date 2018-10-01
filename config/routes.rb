7d49cb01abcf2ef3172b7474719fa039f3b4e89a
oliverochman <oliver.ochman@hotmail.com>
2018-10-01 11:22:37 +0200

    add performance data route

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,10 @@ Rails.application.routes.draw do
   end
 
   namespace :api do
-    namespace :v1 do
+    namespace :v1, defaults: { format: :json } do
       mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
+
+      resources :performance_data, only: [:create]
     end
   end
-end
+endgit 
